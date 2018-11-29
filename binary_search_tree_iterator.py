7e6266112ee995263708e8eeb6f6402761e1c596
Sandeep Shukla <sandeepshukla31@gmail.com>
2018-11-28 16:34:22 -0800

    add O(1) for next()

--- binary_search_tree_iterator.py
+++ binary_search_tree_iterator.py
@@ -7,6 +7,9 @@ Note: next() and hasNext() should run in average O(1) time and uses O(h) memory,
 '''
 
 
+from commons.binary_tree import Node
+
+
 
 class BSTIterator(object):
     def __init__(self, root):
@@ -14,7 +17,7 @@ class BSTIterator(object):
         :type root: TreeNode
         """
         self.stack = []
-        self.add_nodes(root)
+        self.add_nodes(root)  # pre-compute only left nodes of left sub-tree
 
     def hasNext(self):
         """
@@ -30,7 +33,7 @@ class BSTIterator(object):
             node = self.stack.pop()
             self.add_nodes(node.right)
 
-            return node.val
+            return node.data
         else:
             return None
 
@@ -39,3 +42,46 @@ class BSTIterator(object):
             self.stack.append(root)
             root = root.left
 
+class BSTIterator2(object):
+    def __init__(self, root):
+        """
+        :type root: TreeNode
+        """
+        self.stack = []
+        self.add_nodes(root)  # pre-compute every node
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return True if self.stack else False
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        if self.stack:
+            return self.stack.pop().data
+        else:
+            return None
+
+    def add_nodes(self, root):
+        if root is not None:
+            self.add_nodes(root.left)
+            self.stack.append(root)
+            self.add_nodes(root.right)
+
+if __name__=='__main__':
+    root  	     = Node(4);
+    root.left        = Node(2);
+    root.right       = Node(6);
+    root.left.left   = Node(1);
+    root.left.right  = Node(3);
+    root.right.left  = Node(5);
+    root.right.right = Node(7);
+
+    # bti = BSTIterator(root)
+    bti = BSTIterator2(root)
+
+    while bti.hasNext():
+        print bti.next()
