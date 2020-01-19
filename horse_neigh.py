d20ae9c73b77ff625d7a97b8d079626798441625
Sandeep Shukla <sandeepshukla@Sandeeps-MacBook-Pro-2.local>
2020-01-19 10:53:12 -0800

    explain question

--- horse_neigh.py
+++ horse_neigh.py
@@ -1,3 +1,27 @@
+'''
+In real world horses neigh, and you can count them by listening to them.
+
+For this problem you will be given an input string consisting of lowercases letters which represents combination of neigh of different horses.
+
+You need to return an integer corresponding to minimum number of distinct horses which can produce the given sequence.
+
+If the input string is not a combination of valid neigh from different horses return -1.
+
+Example :
+
+Input : "nei"
+Output : -1
+Explanation: Not a valid neigh.
+Input : "neighneigh"
+Output : 1
+Explanation: Single horse yelling neigh two times.
+Input : "neingeighh"
+Output : 2
+Explanation: Second horse can be seen speaking before the first one finished.
+
+'''
+
+
 class Solution(object):
 
     def __init__(self):
