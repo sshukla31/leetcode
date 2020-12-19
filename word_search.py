fad7a5bca12e91819202e31fc249938fdf2f8116
Sandeep Shukla <sandeep@Sandeeps-MBP.hsd1.ca.comcast.net>
2020-12-19 14:45:43 -0800

    update documentation

--- word_search.py
+++ word_search.py
@@ -1,3 +1,64 @@
+'''
+
+79. Word Search
+Medium
+
+4862
+
+214
+
+Add to List
+
+Share
+Given an m x n board and a word, find if the word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cells, where "adjacent" cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+ 
+
+Example 1:
+------------------
+ Ax | Bx | Cx | E |
+------------------
+ S  | F  | Cx | S |
+------------------
+ A  | Dx | Cx | E |
+------------------
+*NOTE: i have marked 'x' with characters denoting the path and word found pattern
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+Example 2:
+
+----------------
+ A | B | C | E |
+----------------
+ S | F | C | Sx|
+----------------
+ A | D | Ex| Ex|
+----------------
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+Output: true
+Example 3:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Output: false
+ 
+
+Constraints:
+
+m == board.length
+n = board[i].length
+1 <= m, n <= 200
+1 <= word.length <= 103
+board and word consists only of lowercase and uppercase English letters.
+'''
+
+
+
+
 class Solution(object):
     def exist(self, board, word):
         """
